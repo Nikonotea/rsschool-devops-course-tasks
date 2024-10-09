1e403a8aa989d37a3874fe6b829a59cb69ef6fe3
Mykola Nechai <nechaino@gmail.com>
2024-10-09 09:49:53 +0200

    Fix backend for tfstate to saparate creation of the bucket

--- main.tf
+++ main.tf
@@ -1,43 +1,22 @@
-# S3 Bucket Resource without ACL management
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "nicko-terraform-state-bucket"
+terraform {
+  required_version = "= 1.9.7"
 
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "Dev"
+  backend "s3" {
+    bucket  = "nicko-terraform-state-bucket"
+    key     = "state/terraform.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
+    acl     = "private"
   }
 
-  lifecycle {
-    prevent_destroy = true # Prevent accidental destruction of the bucket
-  }
-}
-
-# Separate resource for server-side encryption configuration
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256" # Encryption algorithm for the bucket
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 5.70.0"
     }
   }
 }
 
-# Separate resource for versioning with corrected versioning_configuration block
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# Block public access configuration
-resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+provider "aws" {
+  region = "eu-central-1"
 }
