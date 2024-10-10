64a335727068963ab7e8280e8fd1e63f45b3b3cb
Mykola Nechai <nechaino@gmail.com>
2024-10-10 22:14:10 +0200

    Task-1: Add s3 bucket as a backend for state file

---
+++ phase_1/variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_name" {
+  default = "nicko-terraform-state-bucket"
+}
+
+variable "environment" {
+  default = "Dev"
+}
+
+variable "aws_region" {
+  default = "eu-central-1"
+}
---
+++ s3bucket/main.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = var.bucket_name
+  force_destroy = true
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
