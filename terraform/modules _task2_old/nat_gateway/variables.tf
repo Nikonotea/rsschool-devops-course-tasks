7af97ebe952a6c8b7b7fba28a83099ab2e3f3f8f
Mykola Nechai <nechaino@gmail.com>
2024-10-20 19:26:12 +0200

    Remove old files

--- terraform/modules _task2_old/nat_gateway/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "public_subnet_id" {
-  description = "Public Subnet ID where NAT Gateway will be deployed"
-  type        = string
-}
